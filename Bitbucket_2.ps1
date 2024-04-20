0084267fc032557d159a5c332d039920ed9e07e2
rajesh7979 <43139169+rajesh7979@users.noreply.github.com>
2024-04-20 12:45:19 +0530

    Create Bitbucket_2.ps1

---
+++ Bitbucket_2.ps1
@@ -0,0 +1,28 @@
+# Set variables
+$repoUrl = "https://api.bitbucket.org/2.0/repositories/username/repository_name/src/master/folder_path?pagelen=1000"
+$username = "your_username"
+$password = "your_password"
+$outputFolder = "C:\DestinationFolder"
+
+# Base64 encode the username and password for authentication
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
+
+# Function to download files
+function Download-Files($url) {
+    $response = Invoke-RestMethod -Uri $url -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
+    
+    foreach ($file in $response.values) {
+        if ($file.type -eq "commit_directory") {
+            $newUrl = $file.links.self.href + "?pagelen=1000"
+            Download-Files $newUrl
+        } else {
+            $fileUrl = $file.links.self.href
+            $filePath = $file.path -replace "/", "\"
+            $outputFilePath = Join-Path -Path $outputFolder -ChildPath $filePath
+            Invoke-WebRequest -Uri $fileUrl -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -OutFile $outputFilePath
+        }
+    }
+}
+
+# Call the function to start downloading files
+Download-Files $repoUrl
